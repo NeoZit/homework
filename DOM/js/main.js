29cae06149d2820e8295d2c050d254c1f93abe6e
NeoZit <70685742+NeoZit@users.noreply.github.com>
2026-06-03 14:23:54 +0300

    Add files via upload

---
+++ DOM/js/main.js
@@ -0,0 +1,75 @@
+"use strict";
+
+const todoKeys = {
+  id: "id",
+  text: "text",
+  is_completed: "is_completed",
+};
+
+const todos = [];
+
+const errTodoNotFound = (todoId) => `Todo with id ${todoId} not found`;
+
+const getNewTodoId = (todos) =>
+  todos.reduce((maxId, todo) => Math.max(maxId, todo[todoKeys.id]), 0) + 1;
+
+const createTodo = (todos, text) => {
+  const newTodo = {
+    [todoKeys.id]: getNewTodoId(todos),
+    [todoKeys.text]: text,
+    [todoKeys.is_completed]: false,
+  };
+  todos.push(newTodo);
+  return newTodo;
+};
+
+const completeTodoById = (todos, todoId) => {
+  const todo = todos.find((todo) => todo[todoKeys.id] === todoId);
+
+  if (!todo) {
+    console.error(errTodoNotFound(todoId));
+    return todos;
+  }
+  todo[todoKeys.is_completed] = !todo[todoKeys.is_completed];
+  return todo;
+};
+
+const deleteTodoById = (todos, todoId) => {
+  const todoIndex = todos.findIndex((todo) => todo[todoKeys.id] === todoId);
+  if (todoIndex === -1) {
+    console.error(errTodoNotFound(todoId));
+    return todos;
+  }
+  todos.splice(todoIndex, 1);
+  return todos;
+};
+
+const form = document.querySelector(".form");
+const input = document.querySelector(".input");
+const listTodo = document.querySelector(".todos");
+
+const createTodoElement = function (text) {
+  const li = document.createElement("li");
+  li.classList.add("todo");
+  li.innerHTML = `
+  <div class="todo-text">${text}</div>
+          <div class="todo-actions">
+            <button class="button-complete button">&#10004;</button>
+            <button class="button-delete button">&#10006;</button>
+          </div>
+  `;
+  listTodo.append(li);
+};
+
+const handleCreateTodo = function (todos, text) {
+  createTodo(todos, text);
+  createTodoElement(text);
+};
+
+handleCreateTodo(todos, "Новый дейлик");
+handleCreateTodo(todos, "Дейлик 2");
+handleCreateTodo(todos, "Дейлик 3");
+
+// При помощи метода querySelector получаем элементы .form, .input и .todos
+// Создаем функцию createTodoElement(text), которая будет создавать todo в виде разметки
+// Создаем функцию handleCreateTodo(todos, text), которая будет вызывать createTodo и createTodoElement
